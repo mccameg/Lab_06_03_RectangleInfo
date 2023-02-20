a0f33f28992a76939a1d2e8aa4190386b010e684
mccameg <meganmccarren@yahoo.com>
2023-02-19 20:33:35 -0500

    Final

--- src/Main.java
+++ src/Main.java
@@ -18,26 +18,31 @@ public class Main {
 		{
 			length = in.nextDouble(); // if the input is valid, it will be stored in length
 			in.nextLine(); // clears the buffer
+
+			System.out.print("Please enter the width of the rectangle: "); // prompt to tell the user what to enter
+			if (in.hasNextDouble()) //if the input is a valid number, the code in the if block will execute
+			{
+				width = in.nextDouble(); // if the input is valid, it will be stored in length
+				in.nextLine(); // clears the buffer
+
+				area = length * width; // calculates area by multiplying length * width
+				perimeter = length + length + width + width; // calculates perimeter by adding up all sides
+				diagonalLength = Math.sqrt(width * width + length * length); // calculates the diagonal length using Pythagorean theorem
+
+				System.out.println("The area of the rectangle is " + area + " and the perimeter is " + perimeter); // outputs the area and the perimeter
+				System.out.println("The length of the diagonal is " + diagonalLength); // outputs the diagonal length
+			}
+			else // if the input is invalid (not a number), the code in the else block runs. If the first input is invalid, the user does not get a chance to enter the second input
+			{
+				trash = in.nextLine(); // invalid response is stored in trash
+				System.out.println("Must enter a valid number for width: " + trash); // output tells the user that their input was invalid
+			}
 		}
 		else // if the input is invalid (not a number), the code in the else block runs
 		{
 			trash = in.nextLine(); // invalid response is stored in trash
 			System.out.println("Must enter a valid number for length: " + trash); // output tells the user that their input was invalid
 		}
-
-		System.out.print("Please enter the width of the rectangle: "); // prompt to tell the user what to enter
-		if (in.hasNextDouble()) //if the input is a valid number, the code in the if block will execute
-		{
-			width = in.nextDouble(); // if the input is valid, it will be stored in length
-			in.nextLine(); // clears the buffer
-		}
-		else // if the input is invalid (not a number), the code in the else block runs
-		{
-			trash = in.nextLine(); // invalid response is stored in trash
-			System.out.println("Must enter a valid number for width: " + trash); // output tells the user that their input was invalid
-		}
-
-
 
 
     }
